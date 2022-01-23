c4e8beb492f2249b26a15f7c1a9d554377455548
Sandeep Vittala <applesandy33@gmail.com>
2022-01-23 17:22:45 -0500

    pushing final api post logic

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,7 +1,17 @@
 const router = require('express').Router();
 const fs = require("fs");
 const path = require("path");
-const { noteobjects } = require('../../db/db');
+const { noteobjects } = require("../../db/db");
+
+function createNewNote(body, noteobjects) {
+    const newnoteobject = body;
+    noteobjects.push(newnoteobject);
+    fs.writeFileSync(
+      path.join(__dirname, "../../db/db.json"),
+      JSON.stringify({ noteobjects }, null, 2)
+    );
+    return newnoteobject;
+  }
 
 router.get('/notes', (req, res) => {
     res.json(noteobjects);
@@ -9,7 +19,9 @@ router.get('/notes', (req, res) => {
 });
 
 router.post('/notes', (req, res) => {
-
+    req.body.id = noteobjects.length.toString();
+    const note = createNewNote(req.body, noteobjects);
+    res.json(note);
 });
 
 module.exports = router;
